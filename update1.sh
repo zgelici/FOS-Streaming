a30d614e131179886cf2dfb70e626b69a08d8076
zgelici <zgelici@gmail.com>
2015-09-18 00:29:45 +0200

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Tested on Ubuntu Ubuntu 14.04 and 15.04
-# feel free to ask me if you have any question sevan@tyfix.nl 
-# git clone https://github.com/zgelici/FOS-Streaming.git
-# Install chmod 755 update1.sh && ./update1.sh
+# Automated installation for Fos-streaming panel - http://fos-streaming.com/
+# Source: git clone https://github.com/zgelici/FOS-Streaming.git
 
-echo "##UPDATE##"
+echo -n "Updating FOS-Streaming"
 rm -r /usr/src/FOS-Streaming
 cd /usr/local/nginx/html
 mv /usr/local/nginx/html/config.php /tmp/
@@ -20,4 +18,12 @@ mkdir /usr/local/nginx/html/hl
 chmod -R 777 /usr/local/nginx/html/hl
 mkdir /usr/local/nginx/html/cache
 chmod -R 777 /usr/local/nginx/html/cache
-echo "Update finshed"
+curl "http://127.0.0.1:8000/install.php?install"
+curl "http://127.0.0.1:8000/install.php?update" 
+echo "[x]"
+echo "You need to run a manually an update of your database if you have a different port then 8000"
+echo 'run the command curl "http://127.0.0.1:port/install.php?install" and curl "http://127.0.0.1:8000/install.php?update"'
+echo "or just visit with your browser to http://host:port/install.php?install and http://host:port/install.php?update".
+echo "after updating database you can remove /usr/local/nginx/html/install.php "
+
+
